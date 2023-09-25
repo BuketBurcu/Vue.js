b0fd52a17935006ef10f93996099ba4e145472ac
BuketBurcu <mercanbuket58@gmail.com>
2023-09-25 11:43:00 +0300

    added

--- ChatApp-Uygulama-5/chat-app/src/views/Chatroom.vue
+++ ChatApp-Uygulama-5/chat-app/src/views/Chatroom.vue
@@ -2,14 +2,30 @@
   <div>
     <div class="container">
       <NavBar> </NavBar>
+      <NewChatFrom></NewChatFrom>
     </div>
   </div>
 </template>
 
 <script>
 import NavBar from "../components/NavBar.vue";
+import getUser from "../composables/getUser";
+import { watch } from "vue";
+import { useRouter } from "vue-router";
+import NewChatFrom from "../components/NewChatFrom.vue";
 export default {
-  components: { NavBar },
+  components: { NavBar.NewChatFrom },
+  setup() {
+    const { user } = getUser();
+    const router = userRouter();
+
+    watch(user, () => {
+      if (!user.value) {
+        router.push({ name: "welcome" });
+      }
+    });
+    return {};
+  },
 };
 </script>
 
